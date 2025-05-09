97504bdbe1329d10149c3ec32c15b3b12b0480ef
Alberto Muñoz <albertoconil01@gmail.com>
2025-05-09 15:23:25 +0200

    Se añade el archivo con el comando mas retoques

--- acceso-produccion/iptables-rules.sh
+++ acceso-produccion/iptables-rules.sh
@@ -3,8 +3,9 @@
 # Limpiar reglas anteriores
 iptables -F
 
-# Aceptar tráfico entrante solo por el puerto 2222 (SSH)
-iptables -A INPUT -p tcp --dport 2222 -j ACCEPT
+# Aceptar tráfico entrante solo por los puertos 2222 (SSH) y 22 (para acceder a otros contenedores)
+iptables -A INPUT -p tcp --dport 2222 -j ACCEPT  # Permitir SSH al contenedor
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT    # Permitir SSH al servidor de producción
 
 # Aceptar tráfico local (localhost)
 iptables -A INPUT -i lo -j ACCEPT
